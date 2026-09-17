368226e0dbbb30157344a0d5e3a06ed555ff8a32
AcxesLo <steve.klein.98@web.de>
2026-09-17 15:36:18 +0200

    basic structure, added TODO in Main

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+    //TODO
+    /* - webpage with a form (value + "from unit" + "to unit" dropdowns
+       - Submitting the form sends data to the server
+       - Server calculates the result and re-renders the same page with the answers shown
+       - old-school "submit and reload" pattern, not a modern SPA(?)
+     */
+}
