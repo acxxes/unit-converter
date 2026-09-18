1d2e70757b6b000e2f3845758995edbef007d8cb
AcxesLo <steve.klein.98@web.de>
2026-09-18 16:08:28 +0200

    added thymeleaf dependency

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
        - Submitting the form sends data to the server
        - Server calculates the result and re-renders the same page with the answers shown
        - old-school "submit and reload" pattern, not a modern SPA(?)
+       #Need HTML forms and a server that reads from data and renders a template back
      */
 }
