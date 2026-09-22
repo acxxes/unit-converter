61d1f5693e9b01351693e61629340c55a87c8118
AcxesLo <steve.klein.98@web.de>
2026-09-22 16:23:27 +0200

    updated TODO in 'HandlingFormSubmissionApplication'

--- src/main/java/main/HandlingFormSubmissionApplication.java
+++ src/main/java/main/HandlingFormSubmissionApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HandlingFormSubmissionApplication {
 
+    //TODO
+    // - make only the unit visible on the web that the user selected; it shows atm all 3 units in the result
+
     // ./mvnw spring-boot:run start application
 	// curl http://localhost:8080/
 
