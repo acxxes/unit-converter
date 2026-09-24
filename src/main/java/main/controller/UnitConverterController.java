f8948fa74804d787eba2ba9b24c051c4f9470d99
AcxesLo <steve.klein.98@web.de>
2026-09-24 16:35:49 +0200

    changed temperature enum logic structure, added extra request information inside the terminal window

--- src/main/java/main/controller/UnitConverterController.java
+++ src/main/java/main/controller/UnitConverterController.java
@@ -1,5 +1,6 @@
 package main.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import main.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import main.entity.UnitConverter;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Enumeration;
 
 @Controller
 public class UnitConverterController {
@@ -18,10 +22,14 @@ public class UnitConverterController {
         return "unit_converter";
     }
 
-    //TODO
-    // - finish this, i dont understand it yet
     @PostMapping("/unit_converter")
-    public String convert(@ModelAttribute UnitConverter converter, Model model) {
+    public String convert(@ModelAttribute UnitConverter converter,
+                          Model model,
+                          HttpServletRequest request,
+                          @RequestHeader("User-Agent") String userAgent,
+                          @RequestHeader(value = "Accept-Language", required = false) String acceptLanguage) {
+
+        // conversion logic
         double result = 0;
         String formatted;
         StrategyConverter strategyConverter;
@@ -65,8 +73,29 @@ public class UnitConverterController {
         System.out.println("From Unit: " + unitFrom);
         System.out.println("To Unit: " + unitTo);
         System.out.println("--------------");
-        System.out.println(formatted + unitTo);
-        // conversion logic
+        System.out.println(formatted + unitTo + "\n");
+
+        // inspecting the request
+        System.out.println("Request URI: " + request.getRequestURI());
+        System.out.println("Request method: " + request.getMethod());
+        System.out.println("Client IP address: " + request.getRemoteAddr());
+        System.out.println("User-Agent header: " + userAgent);
+        System.out.println("Accept-Language header: " + acceptLanguage);
+
+        // iterate over all headers send from the browser
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName + ": " + request.getHeader(headerName));
+        }
+
+        // iterate over all submitted form parameters (raw, before Spring binds them into `converter`)
+        Enumeration<String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String paramName = paramNames.nextElement();
+            System.out.println("Param: " + paramName + " = " + request.getParameter(paramName));
+        }
+
         return "unit_converter_result";
     }
 }
--- src/main/java/main/enums/TemperatureUnit.java
+++ src/main/java/main/enums/TemperatureUnit.java
@@ -1,23 +1,23 @@
 package main.enums;
 
 public enum TemperatureUnit {
-    C(1.0, 0.0),      // BaseUnit
-    F(5.0 / 9.0, -32 * (5.0 / 9.0)),
-    K(1.0, -273.15);
-
-    private final double scale;
-    private final double offset;
-
-    TemperatureUnit(double scale, double offset) {
-        this.scale = scale;
-        this.offset = offset;
-    }
+    C,
+    F,
+    K;
 
     public double toCelsiusUnit(double value) {
-        return (value + offset) * scale;
+        return switch (this) {
+            case C -> value;
+            case F -> (value - 32) * 5.0 / 9.0;
+            case K -> value - 273.15;
+        };
     }
 
     public double fromCelsiusUnit(double value) {
-        return (value / scale) - offset;
+        return switch (this) {
+            case C -> value;
+            case F -> (value * 9.0 / 5.0) + 32;
+            case K -> value + 273.15;
+        };
     }
 }
